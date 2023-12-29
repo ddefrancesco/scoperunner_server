e03a64e15fb904aa6c2a6a70a23fa061fcaf74eb
ddefrancesco <ddefrancesco@gmail.com>
2023-12-29 17:04:08 +0100

    [test] align command tests

--- handlers/align_handler_test.go
+++ handlers/align_handler_test.go
@@ -4,8 +4,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"strings"
 	"testing"
+
+	scopeparser "github.com/ddefrancesco/scoperunner_server/scopeparser"
 )
 
 func TestAlignCommandHandler(t *testing.T) {
@@ -22,5 +25,45 @@ func TestAlignCommandHandler(t *testing.T) {
 	}
 	if data == nil {
 		t.Errorf("expected not null data  got %v", string(data))
+	}
+}
+
+func Test_setMode(t *testing.T) {
+	type args struct {
+		mode string
+	}
+	tests := []struct {
+		name string
+		args args
+		want scopeparser.AlignMode
+	}{
+
+		{
+			name: "Case AZ",
+			args: args{mode: "altaz"},
+			want: scopeparser.AltAz,
+		},
+		{
+			name: "Case Polar",
+			args: args{mode: "polar"},
+			want: scopeparser.Polar,
+		},
+		{
+			name: "Case Land",
+			args: args{mode: "land"},
+			want: scopeparser.Land,
+		},
+		{
+			name: "Case Error",
+			args: args{mode: "pipppo"},
+			want: "error",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := setMode(tt.args.mode); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("setMode() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
