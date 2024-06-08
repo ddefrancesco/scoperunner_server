b311445798231656470172866b915ccfc3cddf92
ddefrancesco <ddefrancesco@gmail.com>
2024-06-08 18:45:46 +0200

    fix(test): bool env var as string

--- configurations/config_test.go
+++ configurations/config_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-func TestInitEnvConfig(t *testing.T) {
+func TestInitEnvConfigString(t *testing.T) {
 	// Set test environment variables
 	os.Setenv("SCOPE_TEST_ENV_VAR", "test_value")
 	defer os.Unsetenv("SCOPE_TEST_ENV_VAR")
@@ -22,5 +22,23 @@ func TestInitEnvConfig(t *testing.T) {
 	value := viper.GetString("test.env.var")
 	if value != "test_value" {
 		t.Errorf("Expected TEST_ENV_VAR to be 'test_value', but got '%s'", value)
+	}
+}
+
+func TestInitEnvConfigBool(t *testing.T) {
+	// Set test environment variables
+	os.Setenv("SCOPE_TEST_BOOL_ENV_VAR", "false")
+	defer os.Unsetenv("SCOPE_TEST_ENV_VAR")
+
+	// Call the function being tested
+	err := InitEnvConfig()
+	if err != nil {
+		t.Errorf("InitEnvConfig() returned an unexpected error: %v", err)
+	}
+
+	// Check if the environment variable was loaded correctly
+	value := viper.GetBool("test.bool.env.var")
+	if value != false {
+		t.Errorf("Expected TEST_BOOL_ENV_VARto be 'false', but got '%t'", value)
 	}
 }
